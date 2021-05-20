965079d72b21de40b943b5f0676576b5d85db09d
jerry <14075364d@connect.polyu.hk>
2021-05-20 16:28:55 +0800

    download script for oscar

---
+++ oscar_download.py
@@ -0,0 +1,7 @@
+from nlp import load_dataset
+import xtreme_ds
+xtreme_lan = set()
+for task in xtreme_ds.TASK2LANGS:
+    xtreme_lan = xtreme_lan.union([lan for lan in xtreme_ds.TASK2LANGS[task]])
+for lan in xtreme_lan:
+    dataset = load_dataset('oscar', 'unshuffled_deduplicated_'+lan,cache_dir='/gpfs1/scratch/ckchan666/oscar')
