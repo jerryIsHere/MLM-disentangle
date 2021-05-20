53976e942742d5a9cb004d72ee4e0f8939e1dc98
jerry <14075364d@connect.polyu.hk>
2021-05-20 13:48:53 +0800

    update xtreme_download.py

--- xtreme_download.py
+++ xtreme_download.py
@@ -1,8 +1,3 @@
 import xtreme_ds
-for task in xtreme_ds.TASK:
-    try:
-        set_name, subset_name, split = xtreme_ds.TASK[task]['train']
-        xtreme_ds.get_dataset(set_name, subset_name)[split]
-    except:
-        pass
+xtreme_ds.summary()
 xtreme_ds.summary()
