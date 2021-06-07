b73a5f5285ee60f633481508f487dcd1a65cdae5
jerry <14075364d@connect.polyu.hk>
2021-06-07 19:05:00 +0800

    relative import

--- test/test_import.py
+++ test/test_import.py
@@ -1,8 +1,8 @@
-from experinment_util.experinment_config import ExperinmentConfigSerializer
-from experinment_models.multitask_transformer import MultitaskModel
-from experinment_models.disentangled_transformer import XLMRobertaForDisentanglement
-from experinment_datasets import oscar_corpus
-from experinment_datasets import xtreme_ds
+from ..experinment_util.experinment_config import ExperinmentConfigSerializer
+from ..experinment_models.multitask_transformer import MultitaskModel
+from ..experinment_models.disentangled_transformer import XLMRobertaForDisentanglement
+from ..experinment_datasets import oscar_corpus
+from ..experinment_datasets import xtreme_ds
 print(len(xtreme_ds.xtreme_lan))
 print(len(oscar_corpus.family))
 print(len(oscar_corpus.genus))
