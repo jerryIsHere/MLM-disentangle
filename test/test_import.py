637abbc1c8ed1ddd36d336b9c6b466ff564c5962
jerry <14075364d@connect.polyu.hk>
2021-06-07 19:56:17 +0800

    comit

--- test/test_import.py
+++ test/test_import.py
@@ -1,4 +1,4 @@
-from ..experinment_util.experinment_config import ExperinmentConfigSerializer
+from MLM_DISENTANGLE.experinment_util.experinment_config import ExperinmentConfigSerializer
 from ..experinment_models.multitask_transformer import MultitaskModel
 from ..experinment_models.disentangled_transformer import XLMRobertaForDisentanglement
 from ..experinment_datasets import oscar_corpus
