c423a9db587d111751980618b764d2ddb553c055
jerry <14075364d@connect.polyu.hk>
2021-06-10 14:29:21 +0800

    argparse

--- test/test_oscar.py
+++ test/test_oscar.py
@@ -1,19 +1,29 @@
 from datasets import load_dataset
 import time
+import argparse
+
+parser = argparse.ArgumentParser(description="load time of oscar corpus")
+parser.add_argument(
+    "lan",
+    metavar="lan",
+    type=str,
+    help="time limit for this script",
+)
+args = parser.parse_args()
 
 t = time.time()
 load_dataset(
     "oscar",
-    "unshuffled_deduplicated_en",
+    "unshuffled_deduplicated_" + args.lan,
     cache_dir="/gpfs1/scratch/ckchan666/oscar",
+    ignore_verifications=True,
 )
 print(time.time() - t)
 
 t = time.time()
 load_dataset(
     "oscar",
-    "unshuffled_deduplicated_en",
+    "unshuffled_deduplicated_" + args.lan,
     cache_dir="/gpfs1/scratch/ckchan666/oscar",
-    ignore_verifications=True,
 )
 print(time.time() - t)
