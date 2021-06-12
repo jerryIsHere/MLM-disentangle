17d7de5acbec49c82b1825f82a85b2129c885e8b
jerry <14075364d@connect.polyu.hk>
2021-06-12 17:59:37 +0800

    print time

--- data_preparation/build_custom_oscar.py
+++ data_preparation/build_custom_oscar.py
@@ -1,5 +1,9 @@
 from experiment_datasets import oscar_corpus, xtreme_ds
+import time
 
+t = time.time()
 dataset = oscar_corpus.get_custom_corpus()
+print("elapsed time for building corpus")
+print(time.time() - t)
 for i in range(len(xtreme_ds.xtreme_lan)):
     print(dataset[i])
