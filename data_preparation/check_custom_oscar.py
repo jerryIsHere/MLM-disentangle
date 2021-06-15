6b705a21b73349d16809f1afe28ec40006a78311
jerry <14075364d@connect.polyu.hk>
2021-06-15 17:08:37 +0800

    script for display first 80 example of dataset

---
+++ data_preparation/check_custom_oscar.py
@@ -0,0 +1,19 @@
+from experiment_datasets import oscar_corpus, xtreme_ds
+
+print("custom_dataset")
+dataset = oscar_corpus.get_custom_corpus()
+for i in range(len(xtreme_ds.xtreme_lan) * 2):
+    print(xtreme_ds.xtreme_lan[i % len(xtreme_ds.xtreme_lan)])
+    for label in dataset[i]:
+        print(label)
+        print(dataset[i][label])
+
+print("custom_dataset")
+MLMD_ds = oscar_corpus.MLMDisentangleDataset()
+MLMD_ds = iter(MLMD_ds)
+for i in range(len(xtreme_ds.xtreme_lan) * 2):
+    print(xtreme_ds.xtreme_lan[i % len(xtreme_ds.xtreme_lan)])
+    example = next(MLMD_ds)
+    for label in example:
+        print(label)
+        print(example[label])
