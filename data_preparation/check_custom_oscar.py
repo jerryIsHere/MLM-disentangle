2744d46fbd58a67d2ef31a2cb7905ea3580acc69
jerry <14075364d@connect.polyu.hk>
2021-06-16 20:36:34 +0800

    use debug dataset for checking

--- data_preparation/check_custom_oscar.py
+++ data_preparation/check_custom_oscar.py
@@ -1,14 +1,14 @@
 from experiment_datasets import oscar_corpus, xtreme_ds
 
 print("custom_dataset")
-dataset = oscar_corpus.get_custom_corpus()
+dataset = oscar_corpus.get_custom_corpus_debug()
 for i in range(len(xtreme_ds.xtreme_lan) * 2):
     print(xtreme_ds.xtreme_lan[i % len(xtreme_ds.xtreme_lan)])
     for label in dataset[i]:
         print(label)
         print(dataset[i][label])
 
-print("custom_dataset")
+print("original dataset")
 MLMD_ds = oscar_corpus.MLMDisentangleDataset()
 MLMD_ds = iter(MLMD_ds)
 for i in range(len(xtreme_ds.xtreme_lan) * 2):
