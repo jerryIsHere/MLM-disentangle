90f0d7ada2a255f0973908b16c6279505033effb
jerry <14075364d@connect.polyu.hk>
2021-06-17 12:04:03 +0800

    rearrange log format

--- data_preparation/check_custom_oscar.py
+++ data_preparation/check_custom_oscar.py
@@ -1,19 +1,23 @@
 from experiment_datasets import oscar_corpus, xtreme_ds
 
-print("custom_dataset")
 dataset = oscar_corpus.get_custom_corpus_debug()
+MLMD_ds = oscar_corpus.MLMDisentangleDataset()
+MLMD_ds = iter(MLMD_ds)
 for i in range(len(xtreme_ds.xtreme_lan) * 2):
+    print("custom dataset")
     print(xtreme_ds.xtreme_lan[i % len(xtreme_ds.xtreme_lan)])
     for label in dataset[i]:
         print(label)
         print(dataset[i][label])
-
-print("original dataset")
-MLMD_ds = oscar_corpus.MLMDisentangleDataset()
-MLMD_ds = iter(MLMD_ds)
-for i in range(len(xtreme_ds.xtreme_lan) * 2):
+        if label == "masked_tokens" or label == "tokens":
+            print(oscar_corpus.tokenizer.convert_ids_to_tokens(dataset[i][label]))
+    print()
+    print("original dataset")
     print(xtreme_ds.xtreme_lan[i % len(xtreme_ds.xtreme_lan)])
     example = next(MLMD_ds)
     for label in example:
         print(label)
         print(example[label])
+        if label == "masked_tokens" or label == "tokens":
+            print(oscar_corpus.tokenizer.convert_ids_to_tokens(example[label]))
+    print()
