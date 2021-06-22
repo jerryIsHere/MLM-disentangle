9e216d21004b8261d9a470e9c649265698abe6c6
jerry <14075364d@connect.polyu.hk>
2021-06-22 19:37:40 +0800

    init

---
+++ test/check_custom_xtreme.py
@@ -0,0 +1,62 @@
+from experiment_datasets import xtreme_ds
+ds = xtreme_ds.udposTrainDataset()
+for each in ds:
+    pass
+ds = xtreme_ds.udposValidationDataset()
+for each in ds:
+    pass
+ds = xtreme_ds.udposTestDataset()
+for each in ds:
+    pass
+ds = xtreme_ds.panxTrainDataset()
+for each in ds:
+   pass
+ds = xtreme_ds.panxValidationDataset()
+for each in ds:
+   pass
+ds = xtreme_ds.panxTestDataset()
+for each in ds:
+   pass
+ds = xtreme_ds.xnliTrainDataset()
+for each in ds:
+    pass
+ds = xtreme_ds.xnliValidationDataset()
+for each in ds:
+    pass
+ds = xtreme_ds.xnliTestDataset()
+for each in ds:
+    pass
+ds = xtreme_ds.pawsxTrainDataset()
+for each in ds:
+    pass
+ds = xtreme_ds.pawsxValidationDataset()
+for each in ds:
+    pass
+ds = xtreme_ds.pawsxTestDataset()
+for each in ds:
+    pass
+ds = xtreme_ds.xquadTrainDataset()
+for each in ds:
+    pass
+ds = xtreme_ds.xquadValidationDataset()
+for each in ds:
+    pass
+ds = xtreme_ds.xquadTestDataset()
+for each in ds:
+    pass
+ds = xtreme_ds.mlqaTestDataset()
+for each in ds:
+    pass
+ds = xtreme_ds.tydiqaTrainDataset()
+for each in ds:
+    pass
+ds = xtreme_ds.tydiqaTestDataset()
+for each in ds:
+    pass
+ds = xtreme_ds.bucc2018tDataset()
+for each in ds:
+    pass
+ds = xtreme_ds.tatoebaDataset()
+for each in ds:
+    pass
+print("all dataset are error free")
