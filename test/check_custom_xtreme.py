110dda8194cd23b15c142d864bef57bb2da207e4
jerry <14075364d@connect.polyu.hk>
2021-06-25 21:05:41 +0800

    a

--- test/check_custom_xtreme.py
+++ test/check_custom_xtreme.py
@@ -82,7 +82,7 @@ assert i == len(ds) - 1
 ds = xtreme_ds.xquadTrainDataset()
 for i, each in enumerate(ds):
     for j, s_p in enumerate(each["start_positions"]):
-        assert (
+        assert ds.dataset[i]["answers"]["text"][j] in (
             xtreme_ds.tokenizer.convert_tokens_to_string(
                 xtreme_ds.tokenizer.convert_ids_to_tokens(
                     each["tokens"][
@@ -90,7 +90,6 @@ for i, each in enumerate(ds):
                     ]
                 )
             )
-            == ds.dataset[i]["answers"]["text"][j]
         )
 assert i == len(ds) - 1
 ds = xtreme_ds.xquadValidationDataset()
