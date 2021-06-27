e5f61313ec578c402881fd24bb0cc6a7226c95a7
jerry <14075364d@connect.polyu.hk>
2021-06-27 20:19:53 +0800

    s

--- experiment/xquad/xquad_experiment.py
+++ experiment/xquad/xquad_experiment.py
@@ -20,15 +20,18 @@ def normalize_string(txt):
             xtreme_ds.tokenizer(txt).input_ids[1:-1]
         )
     )
-    return tokenized_detokenized
+    return tokenized_detokenized[1:]
 
 
 def normalize_ids(ids):
     tokenized_detokenized = " ".join(xtreme_ds.tokenizer.convert_ids_to_tokens(ids))
-    if re.search("^\▁\([\d\s]+\)$", tokenized_detokenized):  # is number with brancket
-        tokenized_detokenized = "▁" + tokenized_detokenized[2:-1]
+    tokenized_detokenized = tokenized_detokenized[1:]
+    if re.search("^\([\d\s]+\)$", tokenized_detokenized):  # is number with brancket
+        tokenized_detokenized = tokenized_detokenized[1:-1]
         tokenized_detokenized = "".join(tokenized_detokenized.split())
-    return tokenized_detokenized
+    if re.search("^[\d\s]+$", tokenized_detokenized):  # is number with brancket
+        tokenized_detokenized = "".join(tokenized_detokenized.split())
+    return tokenized_detokenized[1:]
 
 
 def qa_build_model(experiment_config_dict, mlm_model_path, task):
